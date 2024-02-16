c05bb48d55c2864c3e684b51de0dae4daa39e27c
Jan Vorisek <jan@vorisek.me>
2024-02-16 14:36:10 +0100

    chore: remove old build dir

--- build/main/EigenValueDynamicSolver.d.ts
+++
@@ -1,8 +0,0 @@
-import { Solver } from "./fem";
-export declare class EigenValueDynamicSolver extends Solver {
-    n: number;
-    tol: number;
-    constructor();
-    assemble(): void;
-    solve(): any;
-}
--- build/main/LinearStaticSolver.d.ts
+++
@@ -1,5 +0,0 @@
-import { Solver } from "./fem";
-export declare class LinearStaticSolver extends Solver {
-    assemble(): void;
-    solve(): void;
-}
